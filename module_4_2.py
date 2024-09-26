1785c73a3722e1d5365429bd6a59b828259489b4
Pahanych <kleymenov-zapchasti@rambler.ru>
2024-09-26 17:21:26 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()   # здесь функция ничего не возвращает, но и ошибку не выдает
+
+inner_function()  # здесь функция не работает
+
