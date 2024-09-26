3979f400e602bb275240116e2a22bcf4c8585399
Pahanych <kleymenov-zapchasti@rambler.ru>
2024-09-26 17:26:35 +0300

    Delete module_4_2.py

--- module_4_2.py
+++
@@ -1,8 +0,0 @@
-def test_function():
-    def inner_function():
-        print("Я в области видимости функции test_function")
-
-    inner_function()   # здесь функция ничего не возвращает, но и ошибку не выдает
-
-inner_function()  # здесь функция не работает
-
