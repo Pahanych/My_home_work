d9bc1130342fa3ce0669b1422ceb6bc7e396fbda
Pahanych <kleymenov-zapchasti@rambler.ru>
2024-10-07 18:01:14 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -2,10 +2,12 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
-         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    if number == 0:
+        return number + 1
+    elif len(str_number) <= 1:
         return first
 
 
-result = get_multiplied_digits(81903)
+result = get_multiplied_digits(80255)
 print(result)
